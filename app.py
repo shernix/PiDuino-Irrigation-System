25b11134989a0505fd4ffcb1053b07a74ce31edb
shernix <eduardo.santiago8@upr.edu>
2020-12-01 04:18:18 -0400

    Plotly graphs. read csv fil. Update images

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/data')
+def data():
+    df = pd.read_csv("data.csv", index_col=0, header=None)
+    return df.to_string()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
